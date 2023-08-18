05a00b64560dfbfb67b422f04ba088d56636c300
kimhyeok <kimmang940715@naver.com>
2023-08-18 21:48:39 +0900

    RequireComponent, AddComponent, GetComponent 차이를 알기위한 테스트 진행전 스크립트 생성

---
+++ Unity3DRnD_00/Assets/GetComponentTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetComponentTest : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rigid = null;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
