fbf2c64f1696a43965bad5ba15255b9b8ec760f3
kimhyeok <kimmang940715@naver.com>
2023-08-20 00:53:37 +0900

    TMP text 변경 추가

--- Unity3DRnD_00/Assets/DiceTest/DiceTest.cs
+++ Unity3DRnD_00/Assets/DiceTest/DiceTest.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class DiceTest : MonoBehaviour
 {
-    
+    [SerializeField] private TextMeshProUGUI text = null;
     public void OnClickShufleDice()
     { 
         int rand = Random.Range(1, 7);
-        Debug.Log(rand);
+        text.text = rand.ToString();
     }
 }
