437843d06832cd9cd388c3ddd06b2582263d28ef
kimhyeok <kimmang940715@naver.com>
2023-08-20 20:25:50 +0900

    3D Dice 돌리기

---
+++ Unity3DRnD_00/Assets/DiceTest/DiceNumberTextScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceNumberTextScript : MonoBehaviour {
+
+	Text text;
+	public static int diceNumber;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = diceNumber.ToString();
+	}
+}
